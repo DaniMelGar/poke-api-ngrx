4259f475c45ddffdb8c043e841708f2d68c26c0d
Francisco María Bono <francisco.bono@dedalus.eu>
2023-04-19 12:00:25 +0200

    boton de logout añadido y color del button del login a rojo

--- src/app/components/nav-var/nav-var.component.ts
+++ src/app/components/nav-var/nav-var.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav-var',
   templateUrl: './nav-var.component.html',
-  styleUrls: ['./nav-var.component.css']
+  styleUrls: ['./nav-var.component.css'],
 })
 export class NavVarComponent {
+  constructor(private authService: AuthService, private route: Router) {}
 
+  logout() {
+    this.authService.logout();
+    this.route.navigateByUrl('/login');
+  }
 }
